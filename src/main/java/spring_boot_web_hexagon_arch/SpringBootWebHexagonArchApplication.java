b4a7bf2cbfad9b987f54d8b1e9c7d844bbe244a6
TitorSpace <titorsjourney@outlook.com>
2026-06-24 17:19:54 +0200

    Start Spring Boot project

---
+++ src/main/java/spring_boot_web_hexagon_arch/SpringBootWebHexagonArchApplication.java
@@ -0,0 +1,13 @@
+package spring_boot_web_hexagon_arch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebHexagonArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebHexagonArchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/spring_boot_web_hexagon_arch/SpringBootWebHexagonArchApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_boot_web_hexagon_arch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebHexagonArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
